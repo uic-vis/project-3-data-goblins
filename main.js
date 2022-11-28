4a2ae997925e915baae26a3ae36aa52d850d36e5
kutsilyo <maryashleytenedor@gmail.com>
2022-11-27 19:58:11 -0600

    changes

--- main.js
+++ main.js
@@ -1,50 +1,30 @@
 function createMap() {
-    var map = L.map('map').setView([37.8, -96], 4); // 4 specifies the zoom
+
+    var map = L.map('map').setView([37.8, -96], 4);
 
     var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
     }).addTo(map);
-    
-    function onEachFeature(feature, layer) {
-        layers.on({
-            mouseover: highlightFeature,
-            mouseout: resetHighLight,
-        });
-    }
-
-    function resetHighlight(e) {
-        geojson.resetSyle(e.target)
-    }
-
-    function highlightFeature(e) {
-        var layer = e.target;
-
-        layer.setStyle({
-            weight: 5,
-            color: '#666',
-            dashArray: '',
-            fillOpacity: 0.7,
-        })
-
-        layer.bringToFront();
-    }
 
     function style(feature) {
         var colorScale = d3.scaleQuantize()
-            .domain([0, 1000])
-            .range(colorbrewer.YlorRd[9]); // output
-
+            .range(colorbrewer.YlOrRd[9])
+            .domain([0, 1000]);
         return {
             fillColor: colorScale(feature.properties.density),
+            weight: 2,
+            opacity: 1,
+            color: 'white',
+            dashArray: '3',
             fillOpacity: 0.7
-        }
+        };
     }
 
-    var geojson = L.geoJson(statesData, {style: style, onEachFeature: onEachFeature}).addTo(map);
 }
 
-function init() {
+
+function init(){
     createMap();
 }
 
-window.onload = init();
+window.onload = init;
